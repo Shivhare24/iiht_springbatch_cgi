87dba0903606f3ac56c06c42c70ad3ee52b1fad8
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-02-06 17:37:46 +0530

    Add SpringBatchHelloWorldJob example

---
+++ labs/SpringBatchHelloWorldJob/src/main/java/com/examples/spring/batch/SpringBatchHelloWorldMain.java
@@ -0,0 +1,33 @@
+package com.examples.spring.batch;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SpringBatchHelloWorldMain {
+
+	public static void main(String[] args) throws Exception {
+
+		// System.out.println("hello");
+		String[] springConfig = { "job-config.xml" };
+
+		// Creating the application context object
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext(springConfig);
+
+		// Creating the job launcher
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+
+		// Creating the job
+		Job job = (Job) context.getBean("helloWorldJob");
+
+		// Executing the JOB
+		JobExecution execution = jobLauncher.run(job, new JobParameters());
+		System.out.println("Exit Status : " + execution.getStatus());
+
+		context.close();
+	}
+
+}
