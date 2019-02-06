9a52fc98e5f61b894502648bc559eecb69679a89
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-02-06 20:35:42 +0530

    Corrected project directory location

---
+++ labs/examples/SpringBatchHelloWorldJob/src/main/java/com/examples/spring/batch/MyTasklet1.java
@@ -0,0 +1,14 @@
+package com.examples.spring.batch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+
+public class MyTasklet1 implements Tasklet {
+
+	public RepeatStatus execute(StepContribution arg0, ChunkContext arg1) throws Exception {
+		System.out.println("Hello This is a sample example of spring batch 2");
+		return RepeatStatus.FINISHED;
+	}
+}
