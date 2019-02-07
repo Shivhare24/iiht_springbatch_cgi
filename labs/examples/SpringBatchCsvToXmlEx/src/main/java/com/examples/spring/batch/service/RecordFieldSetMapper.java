fb98feebef20881656939d2b4647d26f75758bd1
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2019-02-07 06:06:28 +0530

    Add SpringBatchCsvToXml example

---
+++ labs/examples/SpringBatchCsvToXmlEx/src/main/java/com/examples/spring/batch/service/RecordFieldSetMapper.java
@@ -0,0 +1,30 @@
+package com.examples.spring.batch.service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+import com.examples.spring.batch.model.Transaction;
+
+public class RecordFieldSetMapper implements FieldSetMapper<Transaction> {
+	 
+    public Transaction mapFieldSet(FieldSet fieldSet) throws BindException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        Transaction transaction = new Transaction();
+ 
+        System.out.println(fieldSet.getProperties());
+        transaction.setUsername(fieldSet.readString("username"));
+        transaction.setUserId(fieldSet.readInt(1));
+        transaction.setAmount(fieldSet.readDouble(3));
+        String dateString = fieldSet.readString(2);
+        try {
+            transaction.setTransactionDate(dateFormat.parse(dateString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return transaction;
+    }
+}
